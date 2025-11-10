c66d2e9088f2d8a46381a90f292bdf88b6106bf8
Yanis BOUCHAIB <yanisbouchaib28@gmail.com>
2025-11-10 12:21:48 +0100

    code

--- src/main/java/com/exemple/Primenumber.java
+++ src/main/java/com/exemple/Primenumber.java
@@ -2,6 +2,9 @@ package com.exemple;
 
 public class Primenumber {
      public static boolean isPrime(int n){
+        if(n == 0){
+            return false;
+        }
         return false;
      }
 }
